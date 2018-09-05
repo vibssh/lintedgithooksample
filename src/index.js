8f085136058d145215986bbebc8e167e0f3df3ee
Vaibhav Shringarpure <vibs.sh@gmail.com>
2018-09-05 21:07:41 +0100

    Reducer Sample

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
-const myFunction = () => 'Hello World !';
-myFunction();
+const { createStore } = require('redux');
+
+const initialState = {
+  age: 21,
+};
+
+const myReducer = (state = initialState, action) => {
+  const newState = { ...state };
+  switch (action.type) {
+    case 'ADD':
+      Object.assign(newState, state, { age: newState.age += action.value });
+      break;
+    case 'SUB':
+      Object.assign(newState, state, { age: newState.age -= action.value });
+      break;
+    default:
+      return newState;
+  }
+  return newState;
+};
+
+const store = createStore(myReducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({ type: 'ADD', value: 50 });
+store.dispatch({ type: 'SUB', value: 10 });
